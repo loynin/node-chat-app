c60c36608c6d71636e7cea535462139be128175e
Steven Loy <loynin@gmail.com>
2018-11-16 11:14:33 -0800

    Send Welcome Message to new user join the chat and Send Message to all other users the new user just joint the chat room

--- server/server.js
+++ server/server.js
@@ -15,6 +15,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+   
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: 'Welcome to the chat room',
+            createAt: new Date().getTime()
+        });
+
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'New user join',
+            createAt: new Date().getTime()
+        })
+        
+    
+
+
     socket.on('createMessage', (message) => {
         console.log('Create Message', message);
         io.emit('newMessage', {
@@ -22,6 +38,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () =>{
