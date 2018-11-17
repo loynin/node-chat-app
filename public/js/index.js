bc17f6a7cd31d4839d9417d9a57ea9b4bce6062e
Steven Loy <loynin@gmail.com>
2018-11-17 06:37:40 -0800

    Add CSS and Style the Home Page and Handle the "Click Location" button

--- public/js/index.js
+++ public/js/index.js
@@ -28,12 +28,12 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
-
+    let txtMessage = jQuery('[name = message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name = message]').val()
+        text: txtMessage.val()
     }, function(){
-
+        txtMessage.val('');
     })
 })
 
@@ -44,12 +44,16 @@ locationButton.on('click', (e) => {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function(){
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location.');
     })
 })
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log(`createMessage from:${message.from}  text:${message.text}`);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
